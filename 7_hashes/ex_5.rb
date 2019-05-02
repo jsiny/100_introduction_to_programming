ca081aba8ec9444e6888286dddb5dc54efe09f64
Juliette Sinibardy <juju.siny@gmail.com>
2019-05-02 07:20:21 +0000

    Ex anagrams

--- 7_hashes/ex_5.rb
+++ 7_hashes/ex_5.rb
@@ -1,9 +1,11 @@
+# We can use the equivalent to the "has_key?" method: "has_value?"
+
 cat = {
-  name: "Mochi",
+  name: "Pixel",
   age: "2 months",
   color: "black & white",
   coat: "fluffy"
 }
 
-puts "The cat is fluffy!" if cat.include?("fluffy")
-puts "The cat is cute!" if cat.include?("cute")
+puts "The cat is fluffy!" if cat.has_value?("fluffy")
+puts "The cat is cute!" if cat.has_value?("cute")
